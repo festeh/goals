9c2a895bab3f319e80466b7df258d7ce6edb4ecf
Dmitrii Lipin <dimich3d@ya.ru>
2025-07-04 18:29:06 +0200

    add color

--- backend/database/models.go
+++ backend/database/models.go
@@ -21,6 +21,7 @@ type Task struct {
 type Project struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
 	Name      string    `gorm:"not null" json:"name"`
+	Color     string    `gorm:"default:'gray'" json:"color"`
 	Tasks     []Task    `gorm:"foreignKey:ProjectID" json:"tasks"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
