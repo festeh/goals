aa8c7ca9efef3343a855177e8eacbe95eb83e80d
Dmitrii Lipin <dimich3d@ya.ru>
2025-07-11 22:00:42 +0200

    remove tests

--- frontend/test/mocks.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dimaist/services/api_service.dart';
-import 'package:dimaist/services/app_database.dart';
-import 'package:mockito/annotations.dart';
-
-@GenerateMocks([ApiService, AppDatabase])
-void main() {}
