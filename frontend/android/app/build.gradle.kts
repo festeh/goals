a93103a2070c94e23ae6d7b99045730ab4ed0d1c
Dmitrii Lipin <dimich3d@ya.ru>
2025-07-13 20:10:40 +0200

    update target

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.dimaist"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,6 +20,7 @@ android {
 
     defaultConfig {
         minSdk = 25
+        targetSdk = 30
         flavorDimensions += "appType"
         productFlavors {
             create("phone") {
